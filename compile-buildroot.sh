5237e9c5c9a74b03328f37504134c67ab21155ee
Carl Kittelberger <icedream@icedream.pw>
2022-06-06 00:07:20 +0200

    Explicitly enable sshd service.

--- compile-buildroot.sh
+++ compile-buildroot.sh
@@ -5,3 +5,4 @@ tar -c -v -C buildroot/*/output/target/ --owner=root --group=root \
 	$(cat buildroot/2021.02.10/output/build/packages-file-list.txt | grep openssh | tr ',' ' ' | awk '{print $2}') \
 	| \
 sudo ./mount.sh --write tar -xp
+sudo ./mount.sh --write systemctl enable sshd
