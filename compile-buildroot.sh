37f4c7a8233d3cd51da01dec0637a5db85231de1
Carl Kittelberger <icedream@icedream.pw>
2022-06-05 22:42:33 +0200

    Add script to compile buildroot and copy files to rootfs.

---
+++ compile-buildroot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+./clone-buildroot.sh
+make -C buildroot/*/ -j$(nproc)
+tar -c -v -C buildroot/*/output/target/ --owner=root --group=root \
+	$(cat buildroot/2021.02.10/output/build/packages-file-list.txt | grep openssh | tr ',' ' ' | awk '{print $2}') \
+	| \
+sudo ./mount.sh --write tar -xp
