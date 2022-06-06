444a0ce4f96894c5fc62d89cb67df59b2db6ed9c
Carl Kittelberger <icedream@icedream.pw>
2022-06-06 18:36:44 +0200

    Make it easier to include packages in buildroot install.

--- compile-buildroot.sh
+++ compile-buildroot.sh
@@ -1,8 +1,24 @@
 #!/bin/bash -e
+packages=(
+  openssh
+)
+
+filter_package_files() {
+  filter_str=''
+  for package in "${packages[@]}"; do
+    if [ -n "$filter_str" ]; then
+      filter_str="$filter_str"'\|'
+    fi
+    filter_str="$filter_str"'^'"$package,"
+  done
+
+  grep "$filter_str" | tr ',' ' ' | awk '{print $2}'
+}
+
 ./clone-buildroot.sh
 make -C buildroot/*/ -j$(nproc)
 tar -c -v -C buildroot/*/output/target/ --owner=root --group=root \
-	$(cat buildroot/2021.02.10/output/build/packages-file-list.txt | grep openssh | tr ',' ' ' | awk '{print $2}') \
+	$(cat buildroot/*/output/build/packages-file-list.txt | filter_package_files) \
 	| \
 sudo ./mount.sh --write tar -xp
 sudo ./mount.sh --write systemctl enable sshd
