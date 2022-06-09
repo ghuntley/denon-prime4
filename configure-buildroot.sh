8deb0379c760be809f4f7412304f85abe420e5e9
Carl Kittelberger <icedream@icedream.pw>
2022-06-09 05:47:47 +0200

    Use xconfig if DISPLAY available, otherwise use nconfig.

--- configure-buildroot.sh
+++ configure-buildroot.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 #./clone-buildroot.sh
 cp -v buildroot-config/.config buildroot/*/
-make -C buildroot/*/ -j$(nproc) BR2_EXTERNAL=../../buildroot-customizations xconfig
+
+config_target=nconfig
+if [ -n "$DISPLAY" ]
+then
+  config_target=xconfig
+fi
+
+make -C buildroot/*/ -j$(nproc) BR2_EXTERNAL=../../buildroot-customizations "$config_target"
 cp -v buildroot/*/.config buildroot-config
